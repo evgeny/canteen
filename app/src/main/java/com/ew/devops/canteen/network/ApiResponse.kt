ab2b9e78b753bcbefcf89a913331515b11b0425d
Evgeny Zinovyev <e.zinovyev@gmail.com>
2016-11-08 11:31:16 +0100

    update api

--- app/src/main/java/com/ew/devops/canteen/network/ApiResponse.kt
+++ app/src/main/java/com/ew/devops/canteen/network/ApiResponse.kt
@@ -1,14 +1,24 @@
 package com.ew.devops.canteen.network
 
+/**
+ * high-level response for all api requests
+ */
 data class ApiResponse(
         var Error: String,
         var Content: Content,
         var FallbackUrl: String
 )
 
+/**
+ * "newidentitiy" content
+ */
 data class Content(
         val ApiKey: String,
         val UserId: Int,
         val ProfileSecret: String,
         val ProfileToken: String
 )
+
+//data class Content2{
+//    var Categories: Array<String>
+//}
--- app/src/main/java/com/ew/devops/canteen/network/CulinaApi.kt
+++ app/src/main/java/com/ew/devops/canteen/network/CulinaApi.kt
@@ -6,10 +6,28 @@ import retrofit2.http.Headers
 import retrofit2.http.Query
 import rx.Observable
 
-/**
- * https://api.qnips.com/cons/api/Stores/20943/Menu
- */
 interface CulinaApi {
+    /**
+     * https://api.qnips.com/cons/api/Stores/20943/Menu
+     * GET /cons/api/Stores/20924/Menu HTTP/1.1
+     * Accept: application/json
+     * Accept-Charset: UTF-8
+     * Accept-Language: en-US
+     * User-Agent: Primus/2.0.4 (Android 6.0 Build/MASTER; unknown Android SDK built for x86_64)
+     * App-Feature-Set: 1793
+     * App-Brand: Primus
+     * Authorization: 00b2b8e39a2d37fb886ed5301031aed23a
+     * Host: api.qnips.com
+     * Connection: Keep-Alive
+     * Accept-Encoding: gzip
+     * If-None-Match: "2b78a8338a5bafcdea6caa7b85f14b58"
+     *
+     * curl -H 'Accept: application/json' -H 'Accept-Charset: UTF-8' -H 'Accept-Language: en-US'
+     * -H 'User-Agent: Primus/2.0.4 (Android 6.0 Build/MASTER; unknown Android SDK built for x86_64)'
+     * -H 'App-Feature-Set: 1793' -H 'App-Brand: Primus'
+     * -H 'Authorization: 0076f99b67c097928724a5f82bc4aaf86e'
+     * -H 'Host: api.qnips.com' --compressed 'https://api.qnips.com/cons/api/Stores/20924/Menu'
+     */
     @Headers("Accept: application/json", "App-Feature-Set: 1793")
     @GET("cons/api/Stores/20943/Menu")
     fun getMenu(@Header("Authorization") authorization: String): Observable<String>
@@ -20,4 +38,18 @@ interface CulinaApi {
     @Headers("Accept: application/json", "App-Feature-Set: 1793")
     @GET("cons/api/NewIdentity")
     fun getNewIdentitiy(@Query("deviceType") deviceType: String): Observable<ApiResponse>
+
+    //https://104.47.143.161/cons/api/v2/Master?since=1457002509060
+//    GET /cons/api/Stores/20943/Menu HTTP/1.1
+//    Accept: application/json
+//    Accept-Charset: UTF-8
+//    Accept-Language: en-US
+//    User-Agent: Primus/2.0.4 (Android 6.0 Build/MASTER; unknown Android SDK built for x86_64)
+//    App-Feature-Set: 1793
+//    App-Brand: Primus
+//    Authorization: 00ac7c74a22bc75ea26ad34f984df44dff
+//    Host: api.qnips.com
+//    Connection: Keep-Alive
+//    Accept-Encoding: gzip
+
 }
