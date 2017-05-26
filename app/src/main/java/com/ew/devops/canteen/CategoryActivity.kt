025ec3b3c6b54b110be46acb7193ec9d9c307f4f
Evgeny Zinovyev <e.zinovyev@gmail.com>
2017-05-26 07:44:16 +0200

    add value listener for firebase db

--- app/src/main/java/com/ew/devops/canteen/CategoryActivity.kt
+++ app/src/main/java/com/ew/devops/canteen/CategoryActivity.kt
@@ -2,11 +2,16 @@ package com.ew.devops.canteen
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.util.Log
 import com.ew.devops.canteen.presenter.MainActivityPresenter
 import com.ew.devops.canteen.utils.UiUtils
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_category.*
 import kotlinx.android.synthetic.main.content_category.*
+import java.util.HashMap
 import javax.inject.Inject
 
 
@@ -30,14 +35,28 @@ class CategoryActivity : BaseActivity() {
 
         title = presenter.category!!.Name
 
+        val database = FirebaseDatabase.getInstance()
+        val dbRef = database.getReference("review")
+        dbRef.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(error: DatabaseError?) {
+                Log.w("TAG", "Failed to read value.", error?.toException())
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onDataChange(dataSnapshot: DataSnapshot?) {
+                val value = dataSnapshot?.getValue(HashMap::class.java)
+                Log.d("TAG", "Value is: " + value)
+            }
+
+        })
+
         publish.setOnClickListener { view ->
             // Write a message to the database
-            val database = FirebaseDatabase.getInstance()
-            val myRef = database.getReference("review")
+
             val reviewBody = review.text.toString()
             val reviewId = presenter.category!!.Id
 
-            myRef.child(reviewId.toString()).setValue(reviewBody)
+            dbRef.child(reviewId.toString()).setValue(reviewBody)
         }
 
 //        val fab = findViewById(R.id.fab) as FloatingActionButton
